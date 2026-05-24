95ab11f7c33e80187d10374505104906c97be80e
Kiarash Farajzadehahary <kiarashfa@gmail.com>
2026-05-24 12:09:42 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/calculator/',  // matches your repo name — change if different
+  base: '/kialculator/',  // matches your repo name — change if different
 })
